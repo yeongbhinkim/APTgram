0107c74250f77bd8dc36218d39b430bd470f600f
yeongbhinkim <abc282v@naver.com>
2023-08-26 23:07:19 +0900

    폴더규칙

---
+++ apt/src/main/java/com/project/apt/oauth2/mapper/Oauth2Mapper.java
@@ -0,0 +1,19 @@
+package com.project.apt.oauth2.mapper;
+
+/**
+ * @Class Name : com.project.apt.oauth2.Oauth2Mapper.java
+ * @Description : OAUTH2 로그인 인증
+ * @Modification Information
+ * @
+ * @  수정일             수정자              수정내용
+ * @ ----------   ---------   -------------------------------
+ * @ 2023.08.26    김영빈       	  최초생성
+ *
+ * @author 김영빈
+ * @since 2023.08.26
+ * @version 1.0.0
+ * @see
+ *
+ */
+public interface Oauth2Mapper {
+}
---
+++ apt/src/main/java/com/project/apt/oauth2/service/impl/Oauth2ServiceImpl.java
@@ -0,0 +1,20 @@
+package com.project.apt.oauth2.service.impl;
+
+
+/**
+ * @Class Name : com.project.apt.oauth2.Oauth2ServiceImpl.java
+ * @Description : OAUTH2 로그인 인증
+ * @Modification Information
+ * @
+ * @  수정일             수정자              수정내용
+ * @ ----------   ---------   -------------------------------
+ * @ 2023.08.26    김영빈       	  최초생성
+ *
+ * @author 김영빈
+ * @since 2023.08.26
+ * @version 1.0.0
+ * @see
+ *
+ */
+public class Oauth2ServiceImpl {
+}
