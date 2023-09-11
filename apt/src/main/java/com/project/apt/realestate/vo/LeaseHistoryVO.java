b748addd47b1043ba2d5773cb5ed24ad100db2b4
jungdonguk <foodsmith96@naver.com>
2023-09-11 22:42:47 +0900

    0.4v (Open API 매일마다 돌아가는 스케줄러 매서드 완성)

--- apt/src/main/java/com/project/apt/realestate/vo/LeaseHistoryVO.java
+++ apt/src/main/java/com/project/apt/realestate/vo/LeaseHistoryVO.java
@@ -1,12 +1,10 @@
 package com.project.apt.realestate.vo;
 
 import com.project.apt.realestate.dto.LeaseHistoryResponseDto.TbLnOpendataRentV.InnerRow;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+@Table(name = "LEASE_HISTORY")
 @Getter
 @Entity
 @Builder
--- apt/src/main/java/com/project/apt/realestate/vo/TradeHistoryVO.java
+++ apt/src/main/java/com/project/apt/realestate/vo/TradeHistoryVO.java
@@ -1,12 +1,10 @@
 package com.project.apt.realestate.vo;
 
 import com.project.apt.realestate.dto.TradeHistoryResponseDto.TbLnOpendataRtmsV.InnerRow;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+@Table(name = "TRADE_HISTORY")
 @Getter
 @Entity
 @Builder
